3addbc861a007074ea5340adf1c4712fdb70e528
Rahul Bishnoi <rahul.gdr29@gmail.com>
2024-07-13 16:03:58 +0530

    chore: Update database migration and chat module imports

--- database/migrations/20240117043720-init.js
+++ database/migrations/20240117043720-init.js
@@ -3,8 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    try {
+      // await queryInterface.sequelize.query(
+      //   'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";',
+      //   { transaction },
+      // );
+    } catch (error) {
+      console.log('Error in migration', error);
+      throw error;
+    }
   },
 
-  async down(queryInterface, Sequelize) {
-  },
+  async down(queryInterface, Sequelize) {},
 };
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Message } from './entities/message.entity';
+import { ChatGateway } from './chat.gateway';
+import { ChatService } from './chat.service';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Message])],
+  imports: [SequelizeModule.forFeature([Message]),CommonModule],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
