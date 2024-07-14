5b93ee1b2b1dc105c8b9ee80b8181bd818810750
Aryan Agarwal <aryankush025@gmail.com>
2024-07-15 03:19:06 +0530

    Refactor ChatService to improve user and message retrieval efficiency

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -84,17 +84,17 @@ export class ChatService {
 
     // Query to fetch paginated users
     const usersQuery = `
-        SELECT u.name, u.username, u.email, ranked_ids.content AS content,ranked_ids.created_at AS time, ranked_ids.sender_id AS sender_id, ranked_ids.receiver_id AS receiver_id
+        SELECT u.id, u.name, u.username, u.email, u.created_at, u.updated_at, ranked_ids.id AS message_id, ranked_ids.content AS message_content, ranked_ids.created_at AS message_created_at, ranked_ids.updated_at AS message_updated_at, ranked_ids.sender_id AS message_sender_id, ranked_ids.receiver_id AS message_receiver_id
         FROM (
-            SELECT id, sender_id, receiver_id, content, created_at,
+            SELECT id, sender_id, receiver_id, content, created_at, updated_at,
                    ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) AS rn
             FROM (
-                SELECT sender_id AS id, sender_id, receiver_id, content, created_at
+                SELECT sender_id AS id, sender_id, receiver_id, content, created_at, updated_at
                 FROM messages
                 WHERE sender_id = '${currentUserId}'
                    OR receiver_id = '${currentUserId}'
                 UNION
-                SELECT receiver_id AS id, sender_id, receiver_id, content, created_at
+                SELECT receiver_id AS id, sender_id, receiver_id, content, created_at, updated_at
                 FROM messages
                 WHERE sender_id = '${currentUserId}'
                    OR receiver_id = '${currentUserId}'
@@ -118,8 +118,27 @@ export class ChatService {
           ? +totalResult.total_count
           : 0;
 
+      const users = usersResult[0].map((user: any) => ({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        createdAt: user.created_at,
+        updatedAt: user.updated_at,
+        messages: [
+          {
+            id: user.message_id,
+            content: user.message_content,
+            createdAt: user.message_created_at,
+            updatedAt: user.message_updated_at,
+            senderId: user.message_sender_id,
+            receiverId: user.message_receiver_id,
+          },
+        ],
+      }));
+
       return {
-        users: usersResult[0],
+        users,
         total,
         offset,
         limit,
